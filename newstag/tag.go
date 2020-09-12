acb495869d5c82a253fcb4e69e9cfbb025f17daf
Kirill Kuzin <qiwikuz@yandex.ru>
2020-09-12 20:53:46 +0300

    Add foolproof

--- newstag/tag.go
+++ newstag/tag.go
@@ -1,12 +1,15 @@
-//Package newstag has function that reads an input tag and returns it
+//Package newstag contains of the function that reads an input tag and returns it,
+//and also the function that checks language in the input tag
 package newstag
 
 import (
 	"bufio"
 	"fmt"
 	"github/sports-parser/errorpack"
+	"log"
 	"os"
 	"strings"
+	"unicode"
 )
 
 //GetTag reads tag from standart input and returns it
@@ -14,5 +17,18 @@ func GetTag() string {
 	fmt.Println("Enter Sports.ru news tag (in Russian):")
 	sportsTag, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	errorpack.ErrorErr(err)
+
+	TagCheck(sportsTag)
+
 	return strings.TrimRight(sportsTag, "\r\n")
+}
+
+//TagCheck verifies language of the input tag
+func TagCheck(sportsTag string) {
+	checkInput := []rune(sportsTag)
+	for i := range checkInput {
+		if unicode.Is(unicode.Cyrillic, checkInput[i]) != true {
+			log.Fatal("Tag not in Russian")
+		}
+	}
 }
